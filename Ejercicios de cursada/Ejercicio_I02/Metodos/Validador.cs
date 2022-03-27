558d5007e67a14631cb26efb50f00844f9e826e0
gastongayan <gastongayan@gmail.com>
2022-03-27 20:31:17 -0300

    new file:   Ejercicio_I02/Ejercicio_I02/obj/Debug/net5.0/Ejercicio_I02.assets.cache

---
+++ Ejercicios de cursada/Ejercicio_I02/Metodos/Validador.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Metodos
+{
+    public class Validador
+    {
+        public static bool ValidadarRespuesta(string respuesta)
+        {
+            return respuesta == "S" || respuesta == "s";
+        }
+    }
+}
