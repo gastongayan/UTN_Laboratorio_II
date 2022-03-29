21862cf4949b2b9707442f7a5e347a1f8cda210b
gastongayan <gastongayan@gmail.com>
2022-03-28 22:19:52 -0300

    Ejercicio 1 28-03

---
+++ Ejercicios de cursada/Ejercicio_I01_Clase3/Ejercicio_I01_Clase3/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using Prestamos;
+namespace Ejercicio_I01_Clase3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            Cuenta cuenta1 = new Cuenta("Gaston", 2500365);
+            Cuenta cuenta2 = new Cuenta("Guillermina", 4865999);
+
+            cuenta1.Ingresar(265444);
+            cuenta2.Ingresar(65422);
+
+            Console.WriteLine(cuenta1.Mostrar());
+            Console.WriteLine(cuenta2.Mostrar());
+            
+
+            cuenta1.Retirar(45877);
+            cuenta2.Retirar(98874);
+
+            Console.WriteLine(cuenta1.Mostrar());
+            Console.WriteLine(cuenta2.Mostrar());
+
+
+
+            
+
+
+        }
+    }
+}
---
+++ Ejercicios de cursada/Ejercicio_I01_Clase3/Prestamos/Cuenta.cs
@@ -0,0 +1,70 @@
+﻿using System;
+
+namespace Prestamos
+{
+    public class Cuenta
+    {
+        private string titular;
+        private decimal cantidad;
+
+        public Cuenta(string titular, decimal cantidad)
+        {
+            if(!string.IsNullOrEmpty(titular) && cantidad > 0)
+            {
+                SetTitular(titular);
+                SetCantidad(cantidad);
+
+            }
+            
+        }
+
+        public void SetTitular(string titular)
+        {
+            if (!string.IsNullOrEmpty(titular))
+            {
+                this.titular = titular;
+            }
+            
+        }
+
+        public string GetTitular()
+        {
+            return this.titular;
+        }
+
+        public void SetCantidad(decimal cantidad)
+        {
+            if(cantidad > 0)
+            {
+                this.cantidad = cantidad;
+            }
+            
+        }
+
+        public decimal GetCantidad()
+        {
+            return this.cantidad;
+        }
+
+        public string Mostrar()
+        {
+            return $"Nombre del Titular: {this.titular}   Cantidad: {this.cantidad}";
+        }
+
+        public void Ingresar(decimal cantidad)
+        {
+            if(cantidad > 0)
+            {
+                this.cantidad = cantidad;
+            }    
+        }
+
+        public void Retirar(decimal cantidad)
+        {
+            if(cantidad > 0)
+            {
+                this.cantidad -= cantidad;
+            }
+        }
+    }
+}
