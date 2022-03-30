d74f09cbd0bde9ef2d409019791b163b76fe5453
gastongayan <gastongayan@gmail.com>
2022-03-29 21:26:30 -0300

    Ejercicio I01 Clase 4 29-03-2022

---
+++ Ejercicios de cursada/Ejercicio_I01_Clase4/Ejercicio_I01_Clase4/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using Biblioteca;
+
+namespace Ejercicio_I01_Clase4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Sumador sumador = new Sumador();
+            Sumador sumadorB = new Sumador(2);
+            Sumador sumadorC = new Sumador(10);
+
+
+            Console.WriteLine($"La suma de dos long {sumador.Sumar(45, 2)}");
+            Console.WriteLine($"La suma de dos string{sumador.Sumar(" Hola "," Mundo ")}");
+
+            int cantidadSumas = (int)sumador;
+            Console.WriteLine(cantidadSumas);
+            Console.WriteLine(sumadorB + sumador);
+            Console.WriteLine(sumadorB | sumador);
+            Console.WriteLine(sumador | sumadorC);
+
+        }
+    }
+}
